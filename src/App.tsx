90c416f391017ceb3c52b7e7a6e19b3f7e720c87
Daniel Rabin <88159605+docdr22@users.noreply.github.com>
2025-04-02 15:49:38 +0000

    Updating files, codesandbox compadibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import Dashboard from './pages/Dashboard';
-import './styles/index.css';
+import React from "react";
+import Dashboard from "./pages/dashboard";
+import "./styles/index.css";
 
 function App() {
   return (
