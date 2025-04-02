70ce304ff863cb1fc3353bac4e06c63d768b4861
Daniel Rabin <88159605+docdr22@users.noreply.github.com>
2025-04-02 16:08:39 +0000

    Fixed Tailwind version

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// File: vite.config.js
+export default {
+  server: {
+    host: true,
+    strictPort: true,
+    port: 5173,
+    allowedHosts: [
+      "f2mhjr-5173.csb.app", // Your unique CodeSandbox hostname
+    ],
+  },
+};
